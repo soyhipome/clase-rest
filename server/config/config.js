3eaf6fcadfe112de6fd9fbb722e92704258c66aa
Hipolito Miranda Estrella <hipolitomirandaes@mail.com>
2020-06-26 01:17:08 -0500

    Variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -26,7 +26,7 @@ let urlDB;
     if ( process.env.NODE_ENV === 'dev') {
         urlDB ='mongodb://localhost:27017/cafe'
     } else {
-        urlDB = 'mongodb+srv://polisho:ZvzXP8Bx9RzmojUS@cluster0-sp6zb.mongodb.net/cafe'
+        urlDB = process.env.MONGO_URI
     }
 // urlDB = 'mongodb+srv://polisho:ZvzXP8Bx9RzmojUS@cluster0-sp6zb.mongodb.net/cafe'
 
